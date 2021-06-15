2a4a2a8b481f80bd0c2eb96a6d979fa6f8ac052c
sitota49 <se.sitota.ezra@gmail.com>
2021-06-15 10:58:26 +0300

    layout - header and footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
+<div class="container-fluid" >
+  <Header/>
   <h1>Home Page</h1>
+  <Footer />
+</div>
+
+  
 </template>
 
 <script>
+
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
   name: 'Home',
   props: {
    
   },
-  components: {
-   
-  },
+  components: { 
+    Header,
+    Footer},
   data() {
     
   },
@@ -21,5 +30,8 @@ export default {
 </script>
 
 <style scoped>
-
+div{
+  margin: 0;
+  padding: 0;
+}
 </style>
